60e25eee78b6a5610c03c228ced1904b067dbd74
Waqas Khan <wuikhan@gmail.com>
2020-07-02 21:18:57 -0400

    added page load

--- src/test/java/utilties/BaseClass.java
+++ src/test/java/utilties/BaseClass.java
@@ -18,6 +18,8 @@ public class BaseClass {
 			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
 			 System.getProperty("user.dir") + "/Drivers/phantomjs");
 			 driver = new PhantomJSDriver(caps);
+			 driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			 
 //			if (osName.equals("Mac OS X")) {
 //				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver");
@@ -36,7 +38,7 @@ public class BaseClass {
 		} else {
 			System.out.println("Invalid browser");
 		}
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 }
