f9794310f6e3915593ec57e9ab95d87868e80142
Waqas Khan <wuikhan@gmail.com>
2020-07-02 21:23:37 -0400

    updated the driver info

--- src/test/java/utilties/BaseClass.java
+++ src/test/java/utilties/BaseClass.java
@@ -13,32 +13,24 @@ public class BaseClass {
 	public static void setup(String browserName) {
 		String osName = System.getProperty("os.name");
 		if (browserName.equalsIgnoreCase("Chrome")) {
-			 DesiredCapabilities caps = new DesiredCapabilities();
-			 caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
-			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-			 System.getProperty("user.dir") + "/Drivers/phantomjs");
-			 driver = new PhantomJSDriver(caps);
-			 driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			 
-//			if (osName.equals("Mac OS X")) {
-//				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver");
-//				driver = new ChromeDriver();
-//			} else if(osName.equals("Windows 10")) {
-//				System.setProperty("webdriver.chrome.driver",
-//						System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
-//				driver = new ChromeDriver();
-//			} else if (osName.equals("Linux")) {
-//				 DesiredCapabilities caps = new DesiredCapabilities();
-//				 caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
-//				 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-//				 System.getProperty("user.dir") + "/Drivers/phantomjs");
-//				 driver = new PhantomJSDriver(caps);
-//			}
+			if (osName.equals("Mac OS X")) {
+				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver");
+				driver = new ChromeDriver();
+			} else if(osName.equals("Windows 10")) {
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+			} else if (osName.equals("Linux")) {
+				 DesiredCapabilities caps = new DesiredCapabilities();
+				 caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
+				 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+				 System.getProperty("user.dir") + "/Drivers/phantomjs");
+				 driver = new PhantomJSDriver(caps);
+			}
 		} else {
 			System.out.println("Invalid browser");
 		}
-//		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 }
